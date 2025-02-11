8440966c69dbb71b91ed6f107529e087f68ab2f2
amanmmourya <mouryaaman69@gmail.com>
2025-02-11 16:02:39 +0530

    Added homepage and other components with peding gallery css

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,13 +12,13 @@ const Contact = () => {
                 Contact Us to get the best deals
                 </div>
 
-                <div className='flex justify-center items-center ml-10 space-x-5'>
-                    <div>
+                <div className='ml-10 space-x-5 '>
+                    <div className='inline-block'>
                         <div className='small-heading font-medium'>For Vendors</div>
                         <div className='big-text'>sunrisevents01@gmail.com</div>
                         <div className='big-text'>+91 7854239892</div>
                     </div>
-                    <div>
+                    <div className='inline-block'>
                         <div className='small-heading font-medium'>For Customers</div>
                         <div className='big-text'>sunrisevents01@gmail.com</div>
                         <div className='big-text'>+91 7854239892</div>
@@ -28,27 +28,27 @@ const Contact = () => {
                 <div className='small-heading ml-10 font-medium'>Address Details</div>
                 <div className='big-text ml-10'>Near, 110, Aagam Ochid, Nadani-2, Vesu, Surat, Gujarat 395007</div>
             </div>
-            <div className='right-contact ml-[5vw] w-[40vw] '>
+            <div className='right-contact ml-[5vw] w-[40vw]'>
                 <div className='large-heading font-medium text-2xl mb-5 text-[#870f0f]'>
                 Follow us on
                 </div >
                     <div className="instagram flex space-y-2">
-                        <FaInstagram size={28} className='mx-2 big-text1'/><div className='big-text'>Insta id</div>
+                        <FaInstagram size={28} className='mx-2 big-text1'/><div className='big-text inline-block'>Insta id</div>
                     </div>
                     <div className="facebook flex space-y-2">
-                        <FaFacebook size={28} className='mx-2 big-text1'/><div className='big-text'>Facebook id</div>
+                        <FaFacebook size={28} className='mx-2 big-text1'/><div className='big-text inline-block'>Facebook id</div>
                     </div>
                     <div className="twitter flex space-y-2">
-                        <FaTwitter size={28} className='mx-2 big-text1 '/><div className='big-text'>Twitter id</div>
+                        <FaTwitter size={28} className='mx-2 big-text1 '/><div className='big-text inline-block'>Twitter id</div>
                     
                     </div>
                 <div>
 
                 </div>
             </div>
-            <div className='mobile-c w-full flex justify-center items-center '>
+            {/* <div className=' mobile-c w-full flex justify-center items-center '>
                 <div className="mobile-sr h-96 w-96 "></div>
-            </div>
+            </div> */}
 
 
 
@@ -68,10 +68,12 @@ font-weight:700;
 }
 .big-text{
 font-size:2vh;
-padding:5px;
+padding:1vh;
 }
 .big-text1{
-font-size:42}
+font-size:42;
+width:4vh;
+}
 .left-contact{
 padding:10vh;
 }
