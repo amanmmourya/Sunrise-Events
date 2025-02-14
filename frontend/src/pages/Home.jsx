d6775f8d9a2b54276bf36b46f2208bb57f14a137
amanmmourya <mouryaaman69@gmail.com>
2025-02-15 01:30:43 +0530

    Changed a image

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
       title: "Elegant Celebrations",
     },
     {
-      url: "https://images.unsplash.com/photo-1460978812857-470ed1c77af0?q=80&w=2070",
+      url: "https://images.unsplash.com/photo-1469371670807-013ccf25f16a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Magical Moments",
     },
     {
